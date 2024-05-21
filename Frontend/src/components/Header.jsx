b5cbfac006eb8ea134f22c90ae2f5c70c01758f4
Wing Chi Lam <wingchijoe@gmail.com>
2024-05-21 00:01:00 -0400

    Add purchase form

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
+  { name: 'Home', href: '/', current: false },
   { name: 'Powerlifting', href: '/powerlifting', current: false },
   { name: 'CrossFit', href: '/crossfit', current: false },
   { name: 'Calendar', href: '#', current: false },
@@ -13,7 +13,12 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
+
 export default function Header() {
+  // Retrieve the current url path
+  const path = window.location.pathname.substring(1);
+  // strip the initial /
+  console.log(path);
   return (
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
@@ -42,19 +47,23 @@ export default function Header() {
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
-                    {navigation.map((item) => (
+                    {navigation.map((item) => 
+                   { console.log("item:", item.name);
+                    console.log(path);
+                    console.log(item.name == path);
+                    return (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          item.name.toLowerCase() == path ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                           'rounded-md px-3 py-2 text-sm font-medium'
                         )}
                         aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
                       </a>
-                    ))}
+                    )})}
                   </div>
                 </div>
               </div>
