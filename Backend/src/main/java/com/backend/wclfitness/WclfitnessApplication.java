b852aeda261499b28f11d0d6da59f40c799bff37
Wing Chi Lam <wingchijoe@gmail.com>
2024-05-21 19:20:41 -0400

    init backend project

---
+++ Backend/src/main/java/com/backend/wclfitness/WclfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.backend.wclfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WclfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WclfitnessApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/backend/wclfitness/WclfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.wclfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WclfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
